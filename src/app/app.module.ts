85dd7e08bec98d5d6732e9a366d13d0f12a029bb
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-20 13:50:28 -0400

    style

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ServerComponent} from "./server/server.component";
+import {ServerComponent} from './server/server.component';
+import { CliServersComponent } from './cli-servers/cli-servers.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent
+    ServerComponent,
+    CliServersComponent
   ],
   imports: [
     BrowserModule,
