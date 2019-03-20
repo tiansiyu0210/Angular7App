b5d78a7a07c750da2aec1709f43b13541d6da172
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-20 15:57:29 -0400

    event binding

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,6 +9,7 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = 'online';
   allowNewServer = false;
+  serverCreationStatus = 'no server was created';
 
   getServerStatus() {
     return this.serverStatus;
@@ -18,5 +19,14 @@ export class ServerComponent {
     setTimeout( () => {
       this.allowNewServer = true;
     }, 2000);
+  }
+
+  onCreatedServer() {
+    this.serverCreationStatus = 'server was created';
+  }
+
+  onUpdateServerName(event: Event) {
+    console.log(event);
+    this.serverName = (<HTMLInputElement> event.target).value;
   }
 }
