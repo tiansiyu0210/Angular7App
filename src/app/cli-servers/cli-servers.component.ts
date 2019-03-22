2baf12bf8f752d293feefc83313ab15dd190d5e6
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-22 15:07:15 -0400

    ViewEncapsulation

--- src/app/cli-servers/cli-servers.component.ts
+++ src/app/cli-servers/cli-servers.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-cli-servers',
   templateUrl: './cli-servers.component.html',
-  styleUrls: ['./cli-servers.component.css']
+  styleUrls: ['./cli-servers.component.css'],
+  encapsulation: ViewEncapsulation.Emulated// this is default, we don;t havw to add here, other option is NONE and Native
 })
 export class CliServersComponent implements OnInit {
   serversList = [{id: 1, name: 'tian'}, {id: 2, name: 'siyu'}];
