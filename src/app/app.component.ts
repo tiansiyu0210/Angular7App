44c46311df4c546f2c2a10261756c7d46810ca10
tiansiyu0210 <tiansiyu297@gmail.com>
2019-03-26 22:04:52 -0400

    project contious

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  tagName = 'recipe';
+
+  triggerTag(tagname: string){
+    this.tagName = tagname;
+  }
 }
